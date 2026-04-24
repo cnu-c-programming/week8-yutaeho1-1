71da490e463dd4d473a473429c719db82880aa29
yutaeho1 <yut02@naver.com>
2026-04-24 11:17:31 +0900

    Update 2-1.c

---
+++ code/2-1.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+
+    int main() {
+
+        char arr[] = "hello";
+        char *p = "hello";
+
+        arr[0] = 'H';
+        printf("%s\n", arr);
+
+        p[0] = 'H';
+        printf("%s\n", p);
+
+        return 0;
+    }
