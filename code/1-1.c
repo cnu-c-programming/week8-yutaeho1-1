6c958a31f87d2cf5242a8c56c0f5c8ca43383e68
yutaeho1 <yut02@naver.com>
2026-04-24 11:20:28 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,13 +1,14 @@
     #include <stdio.h>
 
     int main() {
+
         char s1[10] = "hi";
         char s2[] = "hi";
         char s3[] = {'h', 'i', '\0'};
 
+        printf("%s\n", s1);
+        printf("%s\n", s2);
+        printf("%s\n", s3);
 
-        printf("s1: %s\n", s1);
-        printf("s2: %s\n", s2);
-        printf("s3: %s\n", s3);
         return 0;
     }
