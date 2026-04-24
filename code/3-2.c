aefaa2ae3b4df6c44309fbffb2bf4881b07e2a6b
yutaeho1 <yut02@naver.com>
2026-04-24 12:20:12 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -3,7 +3,7 @@
 
     int main(int argc, char *argv[]) {
 
-        for (int i = 0; i < argc; i++){
+        for (int i = 1; i < argc; i++){
             for (int j = 0; j < argc-i; j++)
             {
                 if(strcmp(argv[j], argv[j+1]) > 0) {
